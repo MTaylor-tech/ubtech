9505595a7721db99319947079e701dc7bf09a06b
majikpig <majikpig@gmail.com>
2016-06-12 20:17:35 -0400

    Update ledlib.py

--- ledlib.py
+++ ledlib.py
@@ -6,12 +6,8 @@ blue = 16
 green = 19
 yellow = 20
 red = 21
-#red_btn = 23
-#yellow_btn = 24
-#switch = 25
 
 led_pins = [blue, green, yellow, red]
-#button_pins = [red_btn, yellow_btn, switch]
 GPIO.setup(led_pins, GPIO.OUT)
 
 def turn_on(led):
