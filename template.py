aa4995e1f3a15e22c3fb990c8fe2d391538bc6b5
majikpig <majikpig@gmail.com>
2016-06-13 07:49:01 -0400

    Update template.py

--- template.py
+++ template.py
@@ -1,7 +1,25 @@
 #the following lines will allow you to use buttons and leds
 import btnlib as btn
 import ledlib as led
+import time
 
 led.startup()
 
-#to turn on an led, use a command like this:
+#to turn on all leds, use the led.turn_on_all() function:
+led.turn_on_all()
+time.sleep(2)
+#to turn off all:
+led.turn_off_all()
+
+#to turn on a single led, use a command like this:
+led.turn_on(led.red)
+#your choices for leds are led.red, led.yellow, led.green, led.blue
+time.sleep(2)
+#to turn it off:
+led.turn_off(led.red)
+
+#the btn.isOn(btn) function tells you if a particular button is being pressed or if a switch is on
+#your choices for buttons are currently btn.red, btn.yellow, btn.switch
+while (btn.isOn(btn.switch)) :
+  if (btn.isOn(btn.red)):
+    
