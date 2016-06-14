8fbb4552b18b95fca988e3d71c9ad89ff2f434f4
majikpig <majikpig@gmail.com>
2016-06-14 14:32:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #the following lines will allow you to use buttons and leds
 import btnlib as btn
 import ledlib as led
+import time
 
 led.startup()
 
-#put your code here
+while btn.isOn(btn.switch) :
+  #put your code here
+
+print "Goodbye"
+btn.GPIO.cleanup()
